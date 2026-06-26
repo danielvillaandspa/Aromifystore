97bad31bddef7079c29643cf89fb6cf55631c68f
Daniel's Villa&Spa <6sw7bs79tp@privaterelay.appleid.com>
2026-06-26 21:18:37 +0300

    Update ⁠main.jsx

--- ⁠main.jsx
+++ ⁠main.jsx
@@ -1,1 +1,10 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // سيقرأ الكود من ملف App.jsx الذي سننشئه تالياً
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
